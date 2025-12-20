1ea562abedd1c34bd4471ea14dc42e322b4937d2
SMSourov <81343486+SMSourov@users.noreply.github.com>
2025-12-20 16:33:15 +0600

    Player 2.0
    Added some input maps from the project settings, added a jump animation and slightly modified the player.gd file to play appropriate animations.

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -5,20 +5,35 @@ extends CharacterBody2D
 
 @export var SPEED : float = 130.0
 @export var JUMP_VELOCITY : float = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Flip animation
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
+	
+	# Change animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		elif direction != 0:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
